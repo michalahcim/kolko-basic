fd91425b8a776056efa01fc1bbd15c65a412adb1
Michał <michalgawlik3@gmail.com>
2017-09-03 19:55:55 +0200

    FINAL

--- main.py
+++ main.py
@@ -25,6 +25,13 @@ import curses
 from curses import wrapper
 from time import sleep
 
+def wygranko(p):
+    if p == 2:
+        print("Kolka wygraly")
+    elif p ==3:
+        print("Krzyzyki wygraly")
+    else:
+        print("Zakonczono dzialanie programu")
 def main(stdscr):
     stdscr = curses.initscr()
     
@@ -39,6 +46,7 @@ def main(stdscr):
     wt = 5
     ruch = 0 #parzyste koklo, np krzyzyk
     p1=p2=p3=p4=p5=p6=p7=p8=p9 = 0 #sprawdzanie zajetosci
+    global p0 
     
     #plansza
     #win = curses.newwin(height, width, begin_y, begin_x) #y,x (0,0)to lewy gorny rog
@@ -88,50 +96,83 @@ def main(stdscr):
 
     
     while True:
+        
+        #sprawdzanie czy wygranko
+        if p1 is p2 is p3 != 0 or p1 is p4 is p7 !=0:
+            p0 = p1
+            return p0
+            break
+        elif p4 is p5 is p6 != 0 or p2 is p5 is p8 !=0 or p1 is p5 is p9 !=0 or p3 is p5 is p7 !=0:
+            p0 = p5
+            return p0
+            break
+        elif p7 is p8 is p9 !=0 or p3 is p6 is p9 !=0:
+            p0 = p9
+            return p0
+            break
+        
+            
+                
+                
+                
+        
+        
+        #wczytanie klawisza i ewentualne wstawienie symbolu
         key = stdscr.getch()
         
-        if key is 27:
+        if key is 27: #break przy esc
             break
         
         if key is 49:
-            if p1 != 0:
+            if p1 != 0: #czy zajete 
                 continue
-            elif (ruch%2==0):
+            elif (ruch%2==0): #jak parzyste
                 pad1.addch(1,2,ord("O"))
-            else:
+                p1 = 2  #znaczy ze kolko, do sprawdzania czy wygranko
+            else: 
                 pad1.addch(1,2,ord("X"))
+                p1 = 3 #znaczy ze krzyzyk
             pad1.refresh()
-            p1 = 1
+            
             ruch = ruch + 1
+            
         if key is 50:
             if p2 != 0:
                 continue
             elif (ruch%2==0):
                 pad2.addch(1,2,ord("O"))
+                p2=2
             else:
                 pad2.addch(1,2,ord("X"))
+                p2=3
             pad2.refresh()
-            p2=1
+            
             ruch = ruch + 1
+            
         if key is 51:
             if p3 != 0:
                 continue
             elif (ruch%2==0):
                 pad3.addch(1,2,ord("O"))
+                p3=2
             else:
                 pad3.addch(1,2,ord("X"))
+                p3=3
             pad3.refresh()
-            p3=1
+           
             ruch = ruch + 1
+            
         if key is 52:
             if p4 != 0:
                 continue
             elif (ruch%2==0):
                 pad4.addch(1,2,ord("O"))
+                p4=2
             else:
                 pad4.addch(1,2,ord("X"))
+                p4=3
             pad4.refresh()
-            p4=1
+           
             ruch = ruch + 1
             
         if key is 53:
@@ -139,10 +180,12 @@ def main(stdscr):
                 continue
             elif (ruch%2==0):
                 pad5.addch(1,2,ord("O"))
+                p5=2
             else:
                 pad5.addch(1,2,ord("X"))
+                p5=3
             pad5.refresh()
-            p5=1
+            
             ruch = ruch + 1
             
         if key is 54:
@@ -150,10 +193,12 @@ def main(stdscr):
                 continue
             elif (ruch%2==0):
                 pad6.addch(1,2,ord("O"))
+                p6=2
             else:
                 pad6.addch(1,2,ord("X"))
+                p6=3
             pad6.refresh()
-            p6=1
+            
             ruch = ruch + 1
             
         if key is 55:
@@ -161,10 +206,12 @@ def main(stdscr):
                 continue
             elif (ruch%2==0):
                 pad7.addch(1,2,ord("O"))
+                p7=2
             else:
                 pad7.addch(1,2,ord("X"))
+                p7=3
             pad7.refresh()
-            p7=1
+            
             ruch = ruch + 1
             
         if key is 56:
@@ -172,18 +219,23 @@ def main(stdscr):
                 continue
             elif (ruch%2==0):
                 pad8.addch(1,2,ord("O"))
+                p8 = 2
             else:
                 pad8.addch(1,2,ord("X"))
+                p8 = 3
             pad8.refresh()
             ruch = ruch + 1
+            
         if key is 57:
             if p9 != 0:
                 continue
             elif (ruch%2==0):
                 pad9.addch(1,2,ord("O"))
+                p9 = 2
             else:
                 pad9.addch(1,2,ord("X"))
-            p9 = 1
+                p9 = 3
+            
             pad9.refresh()
             ruch = ruch + 1
         
@@ -205,3 +257,4 @@ def main(stdscr):
     curses.endwin()
 
 wrapper(main)  #wraper przywraca terminal do normalnego stanu
+wygranko(p0)
